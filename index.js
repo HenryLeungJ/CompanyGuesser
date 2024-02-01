c063de7733d58406814c7fd7dba7e0cd1d62ad03
HenryLeungJ <leunghenry619@gmail.com>
2024-01-31 22:27:41 -0800

    added back hashing and added back cookies and authentication and fixed bugs

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import bcrypt from 'bcrypt';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy } from 'passport-local';
 
 dotenv.config();
+
+
 
 //color pallet: https://colorhunt.co/palette/3d3b40525cebbfcfe7f8edff
 //logo ninja: https://api-ninjas.com/api/logo
@@ -13,11 +18,26 @@ dotenv.config();
 const app = express();
 const port = 3000;
 const saltRounds = 10;
-var login = false; //del
+
+var current_company = {};
+var current_score = 0;
 
 //middlewear 
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //database
 const db = new pg.Client({
@@ -32,15 +52,10 @@ const db = new pg.Client({
 db.connect();
 
 //variables to keep track of data
-var current_company = {};
-var current_user = {};
-var current_error = "";
-var current_score = 0;
 
 
 let allCompanies = await axios.get("http://588fc30f7458d612002df0d2.mockapi.io/api/v1/companies");
 allCompanies = allCompanies.data;
-console.log(allCompanies.length);
 
 async function getCompany (name) {
     try {
@@ -56,7 +71,14 @@ async function getCompany (name) {
     }
 }
 
-
+async function getScore(id) {
+    try {
+        const result = await db.query("SELECT highscore FROM users WHERE id=$1", [id]);
+        console.log(result.rows);
+    } catch (error) {
+        
+    }
+}
 
 
 
@@ -80,46 +102,20 @@ async function getLeaderboard() {
 
 
 app.get("/", (req, res) => { //sends user to login page
-    current_score = 0;
-    current_company = {};
-    current_user = {};
-    
-    if(current_error!="") {
-        res.render("login.ejs", {error: current_error});
-        current_error = "";
-    }
-    else {
-        res.render("login.ejs");
-    }
+    res.render("login.ejs");
 });
-app.post("/login", async (req, res) => {
-    const name = req.body.name;
-    const password = req.body.password;
-    var trueName = await db.query("SELECT * FROM users WHERE name=$1", [name]);
-    if(trueName.rows[0]) {
-        await bcrypt.compare(password, trueName.rows[0].password, (err, result) => {
-            console.log(result);
-            if (result) {
-                current_user = trueName.rows[0];
-                res.redirect("/play");
-            }
-            else {
-                current_error = "Wrong pasword, try again" ;
-                res.redirect("/");
-            }
-        })
-    }
-    else {
-        current_error = "User does not exist";
-        res.redirect("/"); //User does not exist
-    }
+app.post("/login", (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: "/play",
+        failureRedirect: "/",
+    })(req, res, next);
 });
 
 app.get("/signup", (req, res) => {
     res.render("signup.ejs"); //redirects to signup page which redirects to /play
 });
 app.post("/signup", async (req, res) => {
-    const name = req.body.name;
+    const name = req.body.username;
     const password = req.body.password;
     await bcrypt.hash(password, saltRounds, async (err, result) => { //hashed password
         if (err) {
@@ -129,9 +125,10 @@ app.post("/signup", async (req, res) => {
             try {
                 console.log(name, result);
                 const newUser = await db.query("INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *", [name, result]);
-                current_user = newUser.rows[0];
-                current_user.password = password;
-                res.redirect("/play");
+                req.login(newUser.rows[0], (err) => {
+                    console.log("success");
+                    res.redirect("/play");
+                })
             } catch (error) {
                 console.log(error);
                 res.render("signup.ejs", {error: "Username taken, try again"});
@@ -141,15 +138,16 @@ app.post("/signup", async (req, res) => {
 });
 
 app.get("/play", async (req, res) => { //user authenticated = play, not = backl to login
-    if(current_user.name){
+    if(req.isAuthenticated()){
         var randomNumber = Math.trunc(100 * Math.random());
         await getCompany(allCompanies[randomNumber].name);
         while(current_company==undefined){
             randomNumber = Math.trunc(100 * Math.random());
             await getCompany(allCompanies[randomNumber].name);
         }
-        console.log(current_user);
-        res.render("play.ejs", {selectedCompany: current_company, current_user: current_user, score: current_score}); //current_company has {name, ticker, image}, current_user has {id, name, score}
+        await getScore();
+        console.log(req.user);
+        res.render("play.ejs", {selectedCompany: current_company, current_user: req.user, score: current_score}); //current_company has {name, ticker, image}, current_user has {id, name, score}
     }
     else {
         res.redirect("/");
@@ -166,9 +164,8 @@ app.post("/play", async (req, res) => {
             current_score+=1;
         }
         else {
-            if(current_score > parseInt(current_user.highscore)){
-                await addPoint(current_user.id, current_score);
-                await authenticate(current_user.name, current_user.password);
+            if(current_score > parseInt(req.user.highscore)){
+                await addPoint(req.user.id, current_score);
             }
             current_score = 0;
         }
@@ -177,16 +174,56 @@ app.post("/play", async (req, res) => {
 });
 
 app.get("/logout", async (req, res) => {
-    var current_company = {};
-    var current_user = {};
-    var current_error = "";
-    var current_score = 0;
-    res.redirect("/");
+    req.logout(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      });
 })
 
 app.get("/leaderboard", async (req, res) => {
     var leaderboard = await getLeaderboard();
     res.render("leaderboard.ejs", {leaderboard: leaderboard});
+});
+
+passport.use("local", new Strategy(async function verify (username, password, cb) {
+
+    console.log("hi");
+    var trueName = await db.query("SELECT * FROM users WHERE name=$1", [username]);
+    if(trueName.rows[0]) {
+        bcrypt.compare(password, trueName.rows[0].password, (err, result) => {
+            console.log(result);
+            if (err) {
+                return cb(err);
+            }
+            else {
+
+                if (result) {
+                    return cb(null, trueName.rows[0]);
+                    
+                }
+                else {
+                    return cb(null, "Wrong pasword, try again"); //wrong password
+                    
+                }
+            }
+            
+        })
+    }
+    else {
+        return("User Not Found"); //User does not exist
+        res.redirect("/"); //User does not exist
+    }
+}))
+
+// do not understand whagt cb does !!!!
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 
